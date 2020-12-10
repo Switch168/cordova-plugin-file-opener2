29922c84e7075b540e9ec359640ecd0928e75b2a
momoterraw <andrewvmail@gmail.com>
2020-12-10 13:12:01 -0800

    prev

--- src/ios/FileOpener2.h
+++ src/ios/FileOpener2.h
@@ -29,6 +29,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 @property(nonatomic, strong) UIDocumentInteractionController *controller;
 @property(nonatomic, strong) CDVViewController *cdvViewController;
+@property(nonatomic, retain) NSTimer *isItDoneHelper;
 
 - (void) open: (CDVInvokedUrlCommand*)command;
 
